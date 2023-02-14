497b4051a4909f9cf9bc3d75aa2c066567f023e5
NMGVox <nijeemgreene@gmail.com>
2023-02-14 16:24:23 -0500

    Add full functionality for changing grid size
    In order to achieve the grid resizing functionality, the code that
    initializes the grid was placed inside of a function. This function
    is called on load with default values. The function is also called
    when the user presses the apply button. The values from the slider are
    used to generate a new grid after the old grid is "removed." The
    removeChild method (as seen in the function removeAllSquares) was
    used over innerHTML due to memory leak and security concerns concerning
    that method.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//TODO remove all child nodes from .square container *Done
+//Add color picker
+//Make a clear button
+//Shade functionality
+//Bonus: Make app behave like a brush instead of only mouseover. (push and drag to draw)
+
 let gridsize = 16;
 let grid_arr = [];
 
@@ -5,18 +11,55 @@ function fill(e){
     this.classList.add("fill");
 }
 
-for(let i = 0; i < 16 * 16; i++){
-    grid_arr.push(document.createElement('div'));
-    grid_arr[i].classList.add("paint-square");
-    grid_arr[i].value = i;
+function changeGridSize(e){
+    let slider_label = document.querySelector(".slider-label");
+    slider_label.textContent = `${this.value} x ${this.value}`;
+    gridsize = this.value;
 }
 
-let container = document.querySelector('.square-container');
-grid_arr.forEach(element => {
-    container.appendChild(element);
-});
+//I have to remove all child nodes from .square-container. 2-14-2023
+function removeAllSquares(p){
+    let child = p.firstElementChild;
+    while(child){
+        p.removeChild(child)
+        child = p.firstElementChild;
+    }
+    return;
+}
+
+function initializeGrid(){
+    console.log("Fish");
+    grid_arr = [];
+    for(let i = 0; i < gridsize * gridsize; i++){
+        grid_arr.push(document.createElement('div'));
+        grid_arr[i].classList.add("paint-square");
+        grid_arr[i].value = i;
+    }
+    let container = document.querySelector('.square-container');
+    removeAllSquares(container);
+    let dimension = 600/gridsize;
+    grid_arr.forEach(element => {
+        element.style.height=`${dimension}px`;
+        element.style.width=`${dimension}px`;
+        container.appendChild(element);
+    });
+
+    grid_arr.forEach(box => {
+        box.addEventListener('mouseover', fill);
+    })
+
+}
 
 
-grid_arr.forEach(box => {
-    box.addEventListener('mouseover', fill);
-})
+initializeGrid();
+
+
+
+let grid_slider = document.querySelector(".grid-slider");
+
+grid_slider.addEventListener("input", changeGridSize);
+
+let apply_btn = document.querySelector("#grid-change");
+
+apply_btn.addEventListener('click', initializeGrid);
+
